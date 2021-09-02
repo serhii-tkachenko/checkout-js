cebbc69561bdcea4c7ff0f10d11ad32dad3470b3
serhii.tkachenko <serhii.tkachenko@bigcommerce.com>
2021-09-02 21:15:45 +0300

    feat(payment): BOLT-24 added Bolt account creation checkbox into BoltEmbeddedPaymentMethod component

--- src/app/payment/paymentMethod/BoltCustomForm.tsx
+++ src/app/payment/paymentMethod/BoltCustomForm.tsx
@@ -1,16 +1,45 @@
-import React, { FunctionComponent } from 'react';
+import React, { useMemo, FunctionComponent } from 'react';
+
+import { TranslatedHtml, TranslatedString } from '../../locale';
+import { CheckboxFormField } from '../../ui/form';
 
 export interface BoltCustomFormProps {
     containerId: string;
+    showCreateAccountCheckbox: boolean;
 }
 
-const BoltCustomForm: FunctionComponent<BoltCustomFormProps> = ({ containerId }) => {
+const BoltCreateAccountCheckbox: FunctionComponent = () => {
+    const options = {
+        privacyPolicyUrl: 'https://www.bolt.com/privacy/',
+        termsUrl: 'https://www.bolt.com/end-user-terms/',
+    };
+
+    const labelContent = useMemo(() => (
+        <>
+            <TranslatedString id="payment.bolt_create_account_label" />
+            <br />
+            <TranslatedHtml
+                data={ options }
+                id="payment.bolt_create_account_disclaimer"
+            />
+        </>
+    ), [options]);
+
+    return <CheckboxFormField
+        additionalClassName="form-checkbox form-field--createAccount"
+        labelContent={ labelContent }
+        name="shouldCreateAccount"
+    />;
+};
+
+const BoltCustomForm: FunctionComponent<BoltCustomFormProps> = ({ containerId, showCreateAccountCheckbox }) => {
     return (
         <div className="form-ccFields">
             <div
                 className="form-field form-field--bolt-embed"
                 id={ containerId }
             />
+            { showCreateAccountCheckbox ? <BoltCreateAccountCheckbox /> : null }
         </div>
     );
 };
