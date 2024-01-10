ae2e6969ae9708eedd1fdab5ae89ed96dd8e49ba
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2024-01-10 11:43:23 +0200

    feat(payment): PAYPAL-3405 added PayPalCommerceAcceleratedCheckout payment method

---
+++ packages/paypal-commerce-integration/src/PayPalCommerceAcceleratedCheckout/components/PayPalCommerceAcceleratedCheckoutForm.tsx
@@ -0,0 +1,52 @@
+import React, { FunctionComponent, useEffect } from 'react';
+
+import {
+    usePayPalCommerceAcceleratedCheckoutInstruments
+} from '../hooks/usePayPalCommerceAcceleratedCheckoutInstruments';
+import { PayPalConnectCardComponentRef } from '../PayPalCommerceAcceleratedCheckoutPaymentMethod';
+
+import PayPalCommerceAcceleratedCheckoutCreditCardForm from './PayPalCommerceAcceleratedCheckoutCreditCardForm';
+import PayPalCommerceAcceleratedCheckoutInstrumentsForm from './PayPalCommerceAcceleratedCheckoutInstrumentsForm';
+
+interface PayPalCommerceAcceleratedCheckoutFormProps {
+    renderPayPalConnectCardComponent?: PayPalConnectCardComponentRef['render'];
+    showPayPalConnectCardSelector?: PayPalConnectCardComponentRef['showPayPalConnectCardSelector'];
+}
+
+const PayPalCommerceAcceleratedCheckoutForm: FunctionComponent<
+    PayPalCommerceAcceleratedCheckoutFormProps
+> = ({ renderPayPalConnectCardComponent, showPayPalConnectCardSelector }) => {
+    const {
+        instruments,
+        handleSelectInstrument,
+        selectedInstrument,
+    } = usePayPalCommerceAcceleratedCheckoutInstruments();
+
+    const shouldShowInstrumentsForm = instruments.length > 0;
+
+    useEffect(() => {
+        if (!selectedInstrument && instruments.length > 0) {
+            handleSelectInstrument(instruments[0].bigpayToken);
+        }
+    }, [instruments, selectedInstrument]);
+
+    return (
+        <div className="paymentMethod paymentMethod--creditCard">
+            {/* Add vaulted element here */}
+            {shouldShowInstrumentsForm && (
+                <PayPalCommerceAcceleratedCheckoutInstrumentsForm
+                    onChange={showPayPalConnectCardSelector}
+                    selectedInstrument={selectedInstrument || instruments[0]}
+                />
+            )}
+
+            {!shouldShowInstrumentsForm && (
+                <PayPalCommerceAcceleratedCheckoutCreditCardForm
+                    renderPayPalConnectCardComponent={renderPayPalConnectCardComponent}
+                />
+            )}
+        </div>
+    );
+};
+
+export default PayPalCommerceAcceleratedCheckoutForm;
